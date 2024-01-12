556f4fecb1a4ca4398167a4de55f81cf68a93321
Ricardo <ricardowoj@hotmail.com>
2024-01-12 13:31:03 +0000

    add sort compareDate

--- src/services/useLocalStorage.js
+++ src/services/useLocalStorage.js
@@ -1,6 +1,23 @@
+import {toRaw} from "vue";
 
 export function saveLocalStorage(key, data) {
-  localStorage.setItem(key, JSON.stringify(data));
+  const compareDate = (a, b) => {
+    const [dayA, monthA, yearA] = a.dateCreated.split('/')
+    const dateA = new Date(dayA, monthA, yearA)
+
+    const [dayB, monthB, yearB] = b.dateCreated.split('/')
+    const dateB = new Date(dayB, monthB, yearB)
+
+    if (dateA < dateB) {
+      return -1
+    } else if (dateA > dateB) {
+      return 1
+    } else {
+      return 0
+    }
+  }
+
+  localStorage.setItem(key, JSON.stringify(toRaw(data).sort(compareDate)));
 }
 
 export function getLocalStorage(key) {
